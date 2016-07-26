0b717c983b954c4948a1926365b25eae7164830a
wileys <waileasiler@yahoo.com>
2016-07-26 10:03:44 -0700

    Added basic structure for Python

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
+import pygame
+import random
+
+pygame.init()
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+GREY = (127, 127, 127)
+
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 500
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("CLOSE THE GAP")
+
+done = False
+
+while not done:
+    # main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    pygame.display.flip()
+
+
+pygame.quit()
+exit()
 
 # OUR LOVELY CODE WILL GO HERE.
