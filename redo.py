c4fd2978caf93004287166eb2d2e9c03f9962d9f
BemisEleanor <bemis.eleanor@gmail.com>
2016-07-29 09:34:01 -0700

    finish start screen

--- redo.py
+++ redo.py
@@ -31,7 +31,7 @@ TITLE = "CLOSE THE GAP"
 pg.display.set_caption("CLOSE THE GAP")
 
 
-
+FONT_NAME = 'arial'
 vec = pg.math.Vector2
 
 bgimg = pg.image.load("images/pixelpaper.jpg").convert()
@@ -47,6 +47,7 @@ class Game:
         pg.display.set_caption(TITLE)
         self.clock = pg.time.Clock()
         self.running = True
+        self.font_name = pg.font.match_font(FONT_NAME)
 
     def new(self):
         # start a new game
@@ -89,10 +90,6 @@ class Game:
         if self.player.rect.bottom > HEIGHT:
             self.playing = False 
             pg.time.delay(100)
-
-            
-
-
         
 
     def events(self):
@@ -114,14 +111,32 @@ class Game:
         # *after* drawing everything, flip the display
         pg.display.flip()
 
-    def show_start_screen(self):
-        # game splash/start screen
-        pass
+    def draw_text(self, text, size, color, x, y):
+        font = pg.font.Font(self.font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
 
-    def show_go_screen(self):
-        # game over/continue
-        pass
+    def wait_for_key(self):
+        waiting = True
+        while waiting:
+            self.clock.tick(30)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.running = False
+                if event.type == pg.KEYUP:
+                    waiting = False
 
+    def start_screen(self):
+        self.screen.fill(WHITE)
+        self.draw_text(TITLE, 75, BLACK, WIDTH/2, HEIGHT/4)
+        self.draw_text("Press any key to start game.", 45, BLACK, WIDTH/2, HEIGHT/2)
+        pg.display.flip()
+        self.wait_for_key()
+
+    # def go_screen():
 
 
 class Player(pg.sprite.Sprite):
@@ -145,9 +160,6 @@ class Player(pg.sprite.Sprite):
         self.rect.x -= 1
         if hits:
             self.vel.y = -PLAYER_JUMP
-
-
-
 
 
     def update(self):
@@ -176,10 +188,10 @@ class Platform(pg.sprite.Sprite):
         self.rect.y = y
 
 g = Game()
-g.show_start_screen()
+g.start_screen()
 
 while g.running:  
     g.new()
-    g.show_go_screen()
+    # g.go_screen()
 
 pg.quit()
