c38572fb915111e402c3fddced6720616c746666
wileys <waileasiler@yahoo.com>
2016-08-02 10:44:50 -0700

    Added color of platform

--- redo.py
+++ redo.py
@@ -754,7 +754,7 @@ class Platform(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
         self.image = pg.Surface((w, h))
-        self.image.fill(GREY)
+        self.image.fill((23, 26, 41))
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
